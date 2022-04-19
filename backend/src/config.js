e7204395237edf656ef5ad8e7778c1aaa29ac3d1
perdido101 <mundoperdido101@gmail.com>
2022-04-19 10:45:23 +0300

    t

--- backend/src/config.js
+++ backend/src/config.js
@@ -66,7 +66,7 @@ const ROYALTY_SHARE = 900; // Percentage of the token price that goes to the roy
 const ROYALTY_ADDRESS = "0x1B6888EbC0DB8176B064d285A8d9fda74ec598A4"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = ["0x1B6888EbC0DB8176B064d285A8d9fda74ec598A4"]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
